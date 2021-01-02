e5ebcaa3720c6dea65dc1f292bf14fdc4780f691
pavel.milanov <pawel.milanov@uandex.ru>
2021-01-03 00:25:23 +0300

    исправлена логика возврата значений

--- utils/sports_ru_parse.py
+++ utils/sports_ru_parse.py
@@ -14,15 +14,16 @@ def show_matches():
     match_time = []
     home_team =[]
     guest_team = []
+    link_match = []
     for items in match:
         info_match = items.find_all('td')
         #info_match[0].text # дата
         match_time.append(info_match[1].text) # время
         home_team.append(info_match[-3].text) # домашняя команда
         guest_team.append(info_match[-1].text) # гостевая команда
-    return info_match[0].text, match_time, home_team, guest_team
+    
     for items in match:
         link_match = items.find_all('a')
-        for l in link_match:
-            return "l.get('href')"
+        link_match.append(items.get('href')) # ссылки на матч и команды
+    return info_match[0].text, match_time, home_team, guest_team, link_match
             
