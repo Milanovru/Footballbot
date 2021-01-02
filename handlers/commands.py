773f22b7d27bc5055911863bde6f24aa0449192f
pavel.milanov <pawel.milanov@uandex.ru>
2021-01-03 00:30:21 +0300

    изменена логика под работу с кнопкой

--- handlers/commands.py
+++ handlers/commands.py
@@ -2,7 +2,9 @@ from config import dp
 from aiogram import types
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
+from buttons import football_matches
+from aiogram.types import Message, CallbackQuery
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer("Приветики, {}!".format(message.from_user.full_name))
+    await message.answer("Приветики, {}!".format(message.from_user.full_name), reply_markup=football_matches)
--- handlers/user_text.py
+++ handlers/user_text.py
@@ -3,12 +3,14 @@ from aiogram import types
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
 from utils.sports_ru_parse import show_matches
+from buttons import football_matches
+from aiogram.types import Message, CallbackQuery
 
-@dp.message_handler(text='матчи')
-async def matches(message: types.Message):
-    data, time_list, home_team_list, guest_team_list = show_matches()
-    await message.answer('Расписание матчей на ближайший тур {}'.format(data))
+@dp.callback_query_handler(text='seria_a')
+async def matches(call: CallbackQuery):
+    data, time_list, home_team_list, guest_team_list, link_match = show_matches()
+    await call.answer('Ближайший тур Seria A')
+    await call.message.answer('Расписание матчей на ближайший тур {}'.format(data))
     for time, home, guest in zip(time_list, home_team_list, guest_team_list):
-        await message.answer('{}: {} - {}'.format(time, home, guest))
+        await call.message.answer('{}: {} - {}'.format(time, home, guest))
         
-    
