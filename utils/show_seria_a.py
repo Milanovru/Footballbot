ceacfc804873c28c8a3658029dcc24f94c2060fb
pavel.milanov <pawel.milanov@uandex.ru>
2021-01-03 14:31:26 +0300

    исправлена логика вывода резульатов

--- utils/show_seria_a.py
+++ utils/show_seria_a.py
@@ -5,16 +5,26 @@ def show_matches():
     match_time = []
     home_team =[]
     guest_team = []
-    link_matches = []
     for items in match:
         info_match = items.find_all('td')
         #info_match[0].text # дата
         match_time.append(info_match[1].text) # время
         home_team.append(info_match[-3].text) # домашняя команда
         guest_team.append(info_match[-1].text) # гостевая команда
-    
+    return info_match[0].text, match_time, home_team, guest_team
+
+def show_link_matches():
+    link_matches = []
     for items in match:
         link_match = items.find_all('a')
         for link in link_match:
             link_matches.append(link.get('href')) # ссылки на матч и команды
-    return info_match[0].text, match_time, home_team, guest_team, link_matches
+    return mod_show_link_matches(link_matches)
+
+def mod_show_link_matches(link_matches):
+    tmp_list = []
+    https = 'https://www.sports.ru'
+    for link in link_matches:
+        if not link.find('/',0,1):
+            tmp_list.append(https+link)
+    return tmp_list
