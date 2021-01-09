02d5f73e792ecf45eb9f2e9ea355d8e420f74731
pavel.milanov <pawel.milanov@uandex.ru>
2021-01-09 23:03:05 +0300

    правки

--- conf_data.py
+++ conf_data.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '1434241303:AAF6iZ6lFLTxO6gcjMMr6tbVxj694FENcBU'
+BOT_TOKEN = ''
 PARSE_LINK_ITALY = 'https://www.sports.ru/football/match/italy/'
 PARSE_LINK_NEWS = 'https://www.sports.ru/seria-a/news/'
