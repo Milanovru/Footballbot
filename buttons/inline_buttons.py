916681baff36f2fcfec8f09edc6bc3939309c9f7
pavel.milanov <pawel.milanov@uandex.ru>
2021-02-10 23:27:59 +0300

    изменено меню

--- buttons/inline_buttons.py
+++ buttons/inline_buttons.py
@@ -1,9 +1,10 @@
-
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 
 football_matches = InlineKeyboardMarkup(row_width=2) # клавиатура Серии А
 seria_a = InlineKeyboardButton(text='Seria A 🇮🇹', callback_data='seria_a')
 football_matches.insert(seria_a)
+
 
 Seria_a = InlineKeyboardMarkup(row_width=2)
 show_commands = InlineKeyboardButton(text='Последние новости Seria A', callback_data='show_news')
@@ -13,11 +14,19 @@ Seria_a.add(anons_matches)
 table_seria_a = InlineKeyboardButton(text='Таблица', callback_data='table')
 Seria_a.add(table_seria_a)
 
+
 out_keyboard = InlineKeyboardMarkup()
 cancel= InlineKeyboardButton(text='Назад в меню', callback_data='cancel')
 out_keyboard.add(cancel)
+
 
 full_news = InlineKeyboardMarkup()
 full = InlineKeyboardButton(text='Все новости', callback_data='full')
 full_news.add(full)
 full_news.add(cancel)
+
+
+subscriptions = InlineKeyboardMarkup()
+subs = InlineKeyboardButton(text='Подписаться', callback_data='subscript')
+unsubs = InlineKeyboardButton(text='Отписаться', callback_data='unsubscript')
+subscriptions.add(subs, unsubs)
