4081d2e3683f903e2d1ee3a1738f6f1bc41ff721
pavel.milanov <pawel.milanov@uandex.ru>
2021-02-15 21:21:16 +0300

    добален юзерагент

--- utils/sports_ru_parse.py
+++ utils/sports_ru_parse.py
@@ -2,15 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
+from fake_useragent import UserAgent
 
 
-responce_italy = requests.get(os.getenv('PARSE_LINK_ITALY')).text
-responce_news = requests.get(os.getenv('PARSE_LINK_NEWS')).text
-
+ua = UserAgent(use_cache_server=False, verify_ssl=False)
+headers = {'headers': ua.random}
+responce_italy = requests.get(os.getenv('PARSE_LINK_ITALY'), headers=headers).text
+responce_news = requests.get(
+    os.getenv('PARSE_LINK_NEWS'), headers=headers).text
 
 soup_italy = BeautifulSoup(responce_italy, 'lxml')
 soup_news = BeautifulSoup(responce_news, 'lxml')
-
 
 info = soup_italy.find_all('tbody')
 show_new = soup_news.body.find('div', id='branding-layout').find('div', class_ = "pageLayout").find('div', class_ = "contentLayout js-active").find('div', class_='layout-columns layout-columns_nopaddingTop').find('div', class_='news').find('div')
